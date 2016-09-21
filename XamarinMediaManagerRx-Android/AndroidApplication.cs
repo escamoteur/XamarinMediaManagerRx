c01e7f259166e0b52a3061c851565d450a0c3801
Thomas Burkhart <escamoteur@Mac-mini.fritz.box>
2016-09-21 22:54:52 +0200

    Rename Project to XamarinMediaManagerRx

---
+++ XamarinMediaManagerRx-Android/AndroidApplication.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Android.App;
+using Android.Runtime;
+using ReactiveUI;
+using XamarinMediaManagerRx.Core.ViewModels;
+
+namespace XamarinMediaManagerRx
+{
+    [Application(Label = "XamarinMediaManagerRx-Android")]
+    public class AndroidApplication : Application
+    {
+        AutoSuspendHelper autoSuspendHelper;
+        public AndroidApplication(IntPtr handle, JniHandleOwnership transfer) : base(handle,transfer)
+        {
+        }
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+                        
+            autoSuspendHelper = new AutoSuspendHelper(this);
+            RxApp.SuspensionHost.CreateNewAppState = () => {
+                return new AppBootstrapper();
+            };
+
+            RxApp.SuspensionHost.SetupDefaultSuspendResume();
+        }
+    }
+}
+
